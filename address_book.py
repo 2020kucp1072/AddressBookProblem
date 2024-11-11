e86d32a3498a5432394686c825adc5ff6177207c
Vemula Dileep <vemuladileep082002@gmail.com>
2024-11-11 20:51:36 +0530

    [VEMULA] add: python program to have contact class and its instances

--- address_book.py
+++ address_book.py
@@ -12,9 +12,31 @@ import log
 
 log = log.logger_init('AddressBookProblem')
 
+class Contact:
+    def __init__(self, first_name, last_name, address, city, state, zip_code, phone, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone = phone
+        self.email = email
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}, {self.address}, {self.city}, {self.state}, {self.zip_code}, {self.phone}, {self.email}")
+
+class AddressBook:
+    def __init__(self):
+        self.contacts = {}
+        
+    
     
 class AddressBookMain:
-    
+    def __init__(self):
+        
+      self.address_books ={}
+
     def welcome_message(self):
         '''
         Description:
